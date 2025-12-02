52b7427dac2e0886ac7e0884e60468563f042a1f
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-02 07:06:01 +0200

    customize text-selection, add white accent to the blurred parts

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -2,24 +2,26 @@ import { Logo } from "./Logo";
 import { LanguageSwitcher } from "./LanguageSwitcher";
 export function Header() {
     return (
-        <div className="w-full h-20 rounded-2xl flex justify-between items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-xl
-px-6">
-            <div className="w-[18%]">
-                <Logo />
-            </div>
-
-            <div className="w-[430px] flex justify-between items-center text-[14px] font-semibold text-[#1E1E1E]">
-                <p className="cursor-pointer">About Us</p>
-                <p className="cursor-pointer">Our Units</p>
-                <p className="cursor-pointer">On-Site Facilities</p>
-                <p className="cursor-pointer">Location</p>
-            </div>
-
-            <div className="flex w-[18%] justify-between items-center">
-                <div className="w-[140px] h-[50px] border-[#1E1E1E] border rounded-2xl flex justify-center items-center cursor-pointer">
-                    <p className="text-[14px] font-semibold">Contact Us</p>
+        <div className="w-full h-20 rounded-2xl flex justify-between items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-xl px-6">
+            <div className="absolute inset-0 bg-white/30 rounded-2xl pointer-events-none z-0"></div>
+            <div className="z-9999 w-full h-full rounded-2xl flex justify-between items-center">
+                <div className="w-[18%]">
+                    <Logo />
                 </div>
-                <LanguageSwitcher />
+
+                <div className="w-[430px] flex justify-between items-center text-[14px] font-semibold text-[#1E1E1E]">
+                    <p className="cursor-pointer">About Us</p>
+                    <p className="cursor-pointer">Our Units</p>
+                    <p className="cursor-pointer">On-Site Facilities</p>
+                    <p className="cursor-pointer">Location</p>
+                </div>
+
+                <div className="flex w-[18%] justify-between items-center">
+                    <div className="w-[140px] h-[50px] border-[#1E1E1E] border rounded-2xl flex justify-center items-center cursor-pointer">
+                        <p className="text-[14px] font-semibold">Contact Us</p>
+                    </div>
+                    <LanguageSwitcher />
+                </div>
             </div>
         </div>
     );
--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -4,6 +4,7 @@ export function MainPage() {
     return (
         <div className="w-full h-screen p-10">
             <div className="z-20 relative">
+                <div className="absolute inset-0 bg-white/30 rounded-2xl pointer-events-none z-0"></div>
                 <Header />
                 <div className="w-full h-[450px] rounded-2xl flex flex-col items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-xl px-6 py-6 mt-5">
                     <h1 className="w-[60%] text-[64px] text-center font-bold text-[#1E1E1E] leading-[1.2]">Strategic Location and Modern Living in <span className="text-[#AE7461]">Budapest</span></h1>
@@ -17,12 +18,12 @@ export function MainPage() {
 
                     <div className="flex items-center gap-8 mt-12">
 
-                        <div className="w-[280px] h-18 flex justify-center items-center rounded-2xl bg-[#AE7461] gap-3">
+                        <div className="w-[280px] h-18 flex justify-center items-center rounded-2xl bg-[#AE7461] gap-3 cursor-pointer">
                             <Handshake color="white" size={28} strokeWidth={2.5} />
                             <h3 className="uppercase text-white font-bold text-[24px]">Partnership</h3>
                         </div>
 
-                        <div className="w-[300px] h-18 flex justify-center items-center rounded-2xl border-[#1E1E1E] border-2 gap-3">
+                        <div className="w-[300px] h-18 flex justify-center items-center rounded-2xl border-[#1E1E1E] border-2 gap-3 cursor-pointer">
                             <Eye color="#1E1E1E" size={28} strokeWidth={2.5} />
                             <h3 className="uppercase text-[#1E1E1E] font-bold text-[24px]">Explore Units</h3>
                         </div>
