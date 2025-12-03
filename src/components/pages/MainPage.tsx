21854d7e5f559739a610532ac6ad79415ad2c694
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-03 06:57:52 +0200

    attempt to finish the main page responsive design ( to be reviewed later if needed )

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -2,17 +2,17 @@ import { Header } from "../elements/Header"
 import { Handshake, Eye, ArrowDown } from "lucide-react"
 export function MainPage() {
     return (
-        <div className="w-full h-screen p-4 xl:p-10">
+        <div className="w-full h-screen p-4 lg:p-10">
             <div className="z-20 relative">
                 <Header />
-                <div className="w-full h-[435px] xs:max-sm:h-[415px] md:max-lg:h-[570px] lg:h-[450px] rounded-2xl flex flex-col items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md p-4 md:p-6 mt-6">
+                <div className="w-full h-[435px] xs:max-sm:h-[415px] md:max-lg:h-[570px] lg:max-xl:h-[600px] xl:h-[450px] rounded-2xl flex flex-col items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md p-4 md:p-6 mt-6">
                     <div className="absolute inset-0 bg-white/30 rounded-2xl pointer-events-none z-0"></div>
 
-                    <h1 className="w-full text-[24px] xs:text-[27px] md:text-[64px] md:w-[90%] xl:w-[60%] xl:text-[64px] text-center font-bold text-[#1E1E1E] leading-[1.2] relative z-20">Strategic Location and Modern Living in <span className="text-[#AE7461]">Budapest</span></h1>
+                    <h1 className="w-full text-[24px] xs:text-[27px] md:text-[64px] md:w-[90%] lg:max-xl:w-[70%] xl:w-[60%] xl:text-[64px] text-center font-bold text-[#1E1E1E] leading-[1.2] relative z-20">Strategic Location and Modern Living in <span className="text-[#AE7461]">Budapest</span></h1>
                     <div className="w-full flex flex-col items-center mt-3 relative z-20">
-                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] font-light md:w-[85%] lg:w-[65%] text-center"><span className="font-semibold">StaffPlace</span> offers contemporary accommodation for individual residents and corporate teams in the heart of Budapest.</p>
+                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center"><span className="font-semibold">StaffPlace</span> offers contemporary accommodation for individual residents and corporate teams in the heart of Budapest.</p>
 
-                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] font-light md:w-[85%] lg:w-[65%] text-center mt-2">
+                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center mt-2">
                             Located at <span className="font-semibold">Beke utca 22–26</span>, our four-storey complex features <span className="font-semibold">400</span> modern and secure <span className="font-semibold">living units</span>, designed to provide a convenient, well-managed housing solution where comfort, independence, and safety are the top priorities.
                         </p>
                     </div>
