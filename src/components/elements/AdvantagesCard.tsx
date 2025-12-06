e0213a4b6c07464d65874d11c07d86348e9c6e4d
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-06 06:56:30 +0200

    create responsive design for the advantages cards

--- src/components/elements/AdvantagesCard.tsx
+++ src/components/elements/AdvantagesCard.tsx
@@ -12,14 +12,14 @@ type AdvantagesCardProps = {
 export function AdvantagesCard({ bg, title, advantages }: AdvantagesCardProps) {
     return (
         <div className="w-full h-[480px] lg:w-[550px] lg:h-[450px] relative">
-            <div className="w-[75%] h-full rounded-[10px] absolute left-0 top-0 bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md relative p-4 z-20">
+            <div className="w-full h-[75%] xs:max-sm:h-[70%] md:max-lg:h-[65%] lg:w-[75%] lg:h-[101%] rounded-[10px] absolute left-0 top-[-3px] bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md relative p-4 z-20">
                 <div className="absolute inset-0 bg-white/70 rounded-[10px] pointer-events-none z-2"></div>
                 <div className="w-full h-full flex flex-col items-center relative z-50 lg:p-2">
                     <h3 className="text-[18px] lg:text-[28px] text-[#1E1E1E] font-bold mb-4">{title}</h3>
                     {advantages.map((adv, index) => (
                         <div className="w-full flex gap-2 items-center mb-3" key={index}>
-                            <div className="min-w-[35px] min-h-[35px] flex justify-center items-center">{adv.icon}</div>
-                            <p className="text-[14px] text-[#333]">{adv.text}</p>
+                            <div className="min-w-[25px] min-h-[25px] lg:min-w-[35px] lgmin-h-[35px] flex justify-center items-center">{adv.icon}</div>
+                            <p className="text-[12px] lg:text-[14px] text-[#333]">{adv.text}</p>
                         </div>
                     ))}
                 </div>
--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -5,14 +5,14 @@ export function MainPage() {
         <div className="w-full h-screen p-4 lg:p-10">
             <div className="z-20 relative">
                 <Header />
-                <div className="w-full h-[425px] xs:max-sm:h-[415px] md:max-lg:h-[570px] lg:max-xl:h-[600px] xl:h-[450px] rounded-2xl flex flex-col items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md p-4 md:p-6 mt-6">
+                <div className="w-full h-[425px] xs:max-sm:h-[415px] md:max-lg:h-[410px] lg:max-xl:h-[600px] xl:h-[450px] rounded-2xl flex flex-col items-center bg-[rgba(255, 255, 255, 0.5)] backdrop-blur-md p-4 md:p-6 mt-6">
                     <div className="absolute inset-0 bg-white/30 rounded-2xl pointer-events-none z-0"></div>
 
-                    <h1 className="w-full text-[24px] xs:text-[27px] md:text-[64px] md:w-[90%] lg:max-xl:w-[70%] xl:w-[60%] xl:text-[64px] text-center font-bold text-[#1E1E1E] leading-[1.2] relative z-20">Strategic Location and Modern Living in <span className="text-[#AE7461]">Budapest</span></h1>
+                    <h1 className="w-full text-[24px] xs:text-[27px] md:text-[42px] lg:text-[64px] md:w-[90%] lg:max-xl:w-[70%] xl:w-[60%] xl:text-[64px] text-center font-bold text-[#1E1E1E] leading-[1.2] relative z-20">Strategic Location and Modern Living in <span className="text-[#AE7461]">Budapest</span></h1>
                     <div className="w-full flex flex-col items-center mt-3 relative z-20">
-                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center"><span className="font-semibold">StaffPlace</span> offers contemporary accommodation for individual residents and corporate teams in the heart of Budapest.</p>
+                        <p className="text-[12px] xs:text-[13px] w-full md:text-[14px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center"><span className="font-semibold">StaffPlace</span> offers contemporary accommodation for individual residents and corporate teams in the heart of Budapest.</p>
 
-                        <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center mt-2">
+                        <p className="text-[12px] xs:text-[13px] w-full md:text-[14px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-[65%] text-center mt-2">
                             Located at <span className="font-semibold">Beke utca 22–26</span>, our four-storey complex features <span className="font-semibold">400</span> modern and secure <span className="font-semibold">living units</span>, designed to provide a convenient, well-managed housing solution where comfort, independence, and safety are the top priorities.
                         </p>
                     </div>
@@ -34,7 +34,7 @@ export function MainPage() {
                 </div>
             </div>
 
-            <div className="uppercase text-white text-[14px] md:text-[16px] font-normal relative z-20 w-full flex justify-center items-center mt-4 xs:max-sm:mt-6 md:mt-10 gap-1 md:gap-2 cursor-pointer"><h4>Feel free to learn more</h4> <ArrowDown /></div>
+            <div className="uppercase text-white text-[14px] md:text-[16px] font-normal relative z-20 w-full flex justify-center items-center mt-4 xs:max-sm:mt-6 md:mt-6 lg:mt-10 gap-1 md:gap-2 cursor-pointer"><h4>Feel free to learn more</h4> <ArrowDown /></div>
 
             <div>
                 <img src="/main page bg.webp" alt="main page background" className="h-screen w-full bg-center object-cover xl:w-full xl:max-h-screen absolute top-0 left-0 z-0" />
