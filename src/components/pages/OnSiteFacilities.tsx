9601966ac824311bfff3122267258dffcf97e069
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-07 13:48:48 +0200

    add facilities screen responsive design

--- src/components/pages/OnSiteFacilities.tsx
+++ src/components/pages/OnSiteFacilities.tsx
@@ -35,13 +35,14 @@ export function OnSiteFacilities() {
         <div className="w-full min-h-screen p-4 lg:p-10 flex flex-col items-center relative mt-6 lg:mt-0">
             <h2 className="text-[24px] md:text-[32px] lg:text-[42px] text-[#1E1E1E] font-bold text-center">On-Site Facilities</h2>
             <p className="text-[12px] xs:text-[13px] w-full md:text-[16px] lg:max-xl:text-[20px] font-light md:w-[85%] xl:w-full text-center">Our residence provides many shared amenities and services that ensure a comfortable living experience.</p>
-            <div className="w-full flex justify-between mt-6">
+            <div className="w-full flex justify-between flex-col gap-4 md:max-lg:flex-wrap md:max-lg:justify-center md:flex-row md:max-lg:gap-6 lg:gap-0 mt-6">
                 {facilities.map((facility, index) => (
                     <FacilityCard icon={facility.icon} title={facility.title} description={facility.description} index={index} />
                 ))}
             </div>
             <div className="w-full flex justify-center">
-                <img src="./facilities-illustration-widened.webp" alt="facilities-illustration-widened" className="w-[95%]" />
+                <img src="./facilities-illustration-widened.webp" alt="facilities-illustration-widened" className="w-[95%] hidden md:block md:max-lg:mt-6" />
+                <img src="./facilities-illustration-small.webp" alt="facilities-illustration-widened" className="w-full md:hidden mt-8" />
             </div>
         </div>
     );
